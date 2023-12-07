c08d871b88b8897cdad11496425fd457126b028a
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-07 19:36:53 +0200

    update

---
+++ src/soccer_06_2/Game.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soccer_06_1;
+
+/**
+ *
+ * @author Jada
+ */
+public class Game {
+    public Team homeTeam;
+    public Team awayTeam;
+    public Goal[] goals;
+}
