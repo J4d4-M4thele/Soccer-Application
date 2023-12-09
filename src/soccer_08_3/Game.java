ed2d6c3121f2336df45f9cf9f478974fdd482573
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-09 15:24:38 +0200

    first commit

---
+++ src/soccer_08_3/Game.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soccer_08_2;
+
+import soccer_08_1.*;
+import soccer_07_1.*;
+import soccer_06_2.*;
+import soccer_06_1.*;
+
+/**
+ *
+ * @author Jada
+ */
+public class Game {
+    public Team homeTeam;
+    public Team awayTeam;
+    public Goal[] goals;
+}
