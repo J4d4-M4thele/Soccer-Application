38819a04464eacd8130ff01cdc54e1e13dd8728e
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-12-14 09:53:12 +0200

    update

---
+++ src/soccer_12_2/Possession.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soccer_12_2;
+
+/**
+ *
+ * @author Jada
+ */
+public class Possession extends GameEvent {
+    
+}
